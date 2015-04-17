c3c3ed25c4c1870be7299845587517e5e97137b3
Kevin KIN-FOO <kkinfoo@sierrawireless.com>
2015-04-17 16:40:18 +0200

    Using `forms` generation.

--- routes/causerie-edit.js
+++ routes/causerie-edit.js
@@ -1,24 +1,90 @@
 var express = require('express');
 var router = express.Router();
+
 var models = require('../models');
 var getSlug = require('speakingurl');
 
+var forms = require('forms');
+var fields = forms.fields;
+var validators = forms.validators;
+var widgets = forms.widgets;
+
 router.get('/causerie_add', function(req, res) {
-	res.render('causerie-edit');
+  res.render('causerie-edit', {
+    form: causerieForm().toHTML(bootstrapField)
+  });
 });
 
 router.post('/causerie_add', function(req, res) {
-	models.Causerie.create({
-		slug: getSlug(req.body.title),
-		title: req.body.title,
-		description: req.body.description
-	}).catch(function(error) {
-		res.render('causerie-edit', {
-			error: error
-		});
-	}).then(function() {
-		res.redirect('/');
-	});
+  // Validate form
+  cform = causerieForm(req.body);
+  cform.handle(req, {
+    success: function(form) {
+      // Save Causerie
+      models.Causerie.create({
+        slug: getSlug(form.data.title),
+        title: form.data.title,
+        description: form.data.description
+      }).catch(function(error) {
+        causerieFormOnError(cform, res, error);
+      }).then(function() {
+        // All good
+        res.redirect('/');
+      });
+    },
+    error: function(form) {
+      causerieFormOnError(form, res);
+    }
+  });
 });
+
+// Define form accordingly to model
+var causerieForm = function(values) {
+  var cform = forms.create({
+    title: fields.string({
+      required: true,
+      value: values ? values.title : null,
+      placeholder: 'Title',
+      errorAfterField: true,
+      widget: widgets.text({
+        classes: ['input-lg']
+      })
+    }),
+    description: fields.string({
+      required: true,
+      value: values ? values.description : null,
+      errorAfterField: true,
+      widget: widgets.textarea()
+    })
+  });
+  return cform;
+};
+
+// Provide boot strap look to generated form
+var bootstrapField = function(name, object) {
+  object.widget.classes = object.widget.classes || [];
+  object.widget.classes.push('form-control');
+
+  var label = object.labelHTML(name);
+  var error = object.error ? '<div class="alert alert-error help-block">' +
+    object.error + '</div>' : '';
+
+  var validationclass = object.value && !object.error ? 'has-success' : '';
+  validationclass = object.error ? 'has-error' : validationclass;
+
+  var widget = object.widget.toHTML(name, object);
+  return '<div class="form-group ' + validationclass + '">' + label + widget +
+    error + '</div>';
+};
+
+// Handle all kind of form errors
+var causerieFormOnError = function(form, res, error) {
+  var errorHTML = (error || '') + form.fields.title.errorHTML() +
+    form.fields.description.errorHTML();
+  res.render('causerie-edit', {
+    error: errorHTML,
+    form: form.toHTML(bootstrapField)
+  });
+};
 
 module.exports = router;
