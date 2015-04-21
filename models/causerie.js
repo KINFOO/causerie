4b5e465b9e2dca0afddfd4130617bf1efe895509
Kevin KIN-FOO <kkinfoo@sierrawireless.com>
2015-04-21 11:55:26 +0200

    Formatting with jsfmt.

--- models/causerie.js
+++ models/causerie.js
@@ -1,27 +1,25 @@
-var models = require('../models');
-
 module.exports = function(sequelize, DataTypes) {
-	var Causerie = sequelize.define('Causerie', {
-		slug: {
-			type: DataTypes.STRING,
-			primaryKey: true,
-			allowNull: false
-		},
-		title: {
-			type: DataTypes.STRING,
-			allowNull: false
-		},
-		description: {
-			type: DataTypes.TEXT,
-			allowNull: false
-		}
-	}, {
-		freezeTableName: true,
-		classMethods: {
-			associate: function(models) {
-				Causerie.hasMany(models.Post);
-			}
-		}
-	});
-	return Causerie;
+  var Causerie = sequelize.define('Causerie', {
+    slug: {
+      type: DataTypes.STRING,
+      primaryKey: true,
+      allowNull: false
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    description: {
+      type: DataTypes.TEXT,
+      allowNull: false
+    }
+  }, {
+    freezeTableName: true,
+    classMethods: {
+      associate: function(models) {
+        Causerie.hasMany(models.Post);
+      }
+    }
+  });
+  return Causerie;
 };
--- models/post.js
+++ models/post.js
@@ -1,24 +1,18 @@
 module.exports = function(sequelize, DataTypes) {
-	var Post = sequelize.define('Post', {
-		author: {
-			type: DataTypes.STRING
-		},
-		content: {
-			type: DataTypes.TEXT
-		},
-		// Set Foreign Key with `Causerie`
-		causerie_id: {
-			type: DataTypes.STRING,
-			references: "Causerie",
-			referencesKey: "slug"
-		}
-	}, {
-		classMethods: {
-			associate: function(models) {
-				Post.belongsTo(models.Causerie);
-			}
-		},
-		freezeTableName: true
-	});
-	return Post;
+  var Post = sequelize.define('Post', {
+    author: {
+      type: DataTypes.STRING
+    },
+    content: {
+      type: DataTypes.TEXT
+    }
+  }, {
+    classMethods: {
+      associate: function(models) {
+        Post.belongsTo(models.Causerie);
+      }
+    },
+    freezeTableName: true
+  });
+  return Post;
 };
